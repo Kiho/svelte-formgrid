70fa14395893280f51971f3cba991ccdd9259e87
Kiho <kihoc22@gmail.com>
2018-01-30 00:19:21 -0800

    Cleanup computed props

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,30 @@ export function makeUniqueId() {
         return v.toString(16);
     });
 }
+
+function differs(a, b) {
+    return (a !== undefined && a !== b);
+}
+
+function getDiff(newData, oldData) {
+    const diff = {}
+    let changed = false;
+    for (let key in newData) {
+        if (key === name) continue;
+        const val = newData[key]
+        if (differs(oldData[key], val)) {
+            changed = true;
+            diff[key] = newData[key];
+        }
+    }
+    return { diff, changed }
+}
+
+export function mergeProps(component, name) {
+    const props = component.get(name);
+    if (!props) return;
+    const { diff, changed } = getDiff(props, component.get());
+    if (changed) {
+        component.set(diff);
+    }
+}
