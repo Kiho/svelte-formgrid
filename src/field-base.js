fa1571c42413dfbd3a43c4723950ca6640ed38c8
Kiho <kihoc22@gmail.com>
2018-02-04 11:23:32 -0800

    Refactor - move Filed component out

---
+++ src/field-base.js
@@ -0,0 +1,35 @@
+import { makeUniqueId, mergeProps } from './utils';
+
+export default {
+    data() {
+        return { 
+            type: 'text',
+            uuid: makeUniqueId(),
+            placeholder: '',
+            label: '',
+            inputClass: '',
+            value: '',
+            class: '',
+            readOnly: false,
+            required: false,
+            pattern: '',
+            validate: null,
+            submit: false,
+            error: '',
+        }
+    },
+    oncreate(p) {
+        const element = p.refs.input;
+        element.onkeyup = (e) => {
+            if (p.get('submit')) {
+                const error = element.checkValidity() ? '' : element.validationMessage;
+                p.set({error});
+            }
+        };
+        element.setError = (error) => {
+            p.set({error, submit: true});
+        };
+
+        mergeProps(p, 'settings');
+    },
+}
