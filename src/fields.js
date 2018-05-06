1fa95314a4af91ba4985cf96254d7e434572a0f7
Kiho <kihoc22@gmail.com>
2018-05-05 19:01:39 -0700

    Code cleanup

--- src/fields.js
+++ src/fields.js
@@ -3,43 +3,41 @@ import Field from './Field.html';
 import fieldBase from './inputs/field-base';
 import { TextInput, NumberInput, MaskedInput, CurrencyInput, SelectInput } from './inputs';
 
-function mergeComponentState(base, data) {
+function mergeState(base, data) {
 	return Object.assign({}, base, { settings: base }, { fieldtype: TextInput}, data);
 }
 
-export const TextField = class extends Field {    
-    constructor(options) {
-		options.data = mergeComponentState(options.data,  { fieldtype: TextInput });
-		// console.log('Text constructor(options) ', options);	
-		super(options);
-		// console.log('Text = class extends Field', this._state);
-	}    
-}
-
-export const NumberField = class extends Field {    
-    constructor(options) {
-		options.data = mergeComponentState(options.data,  { fieldtype: NumberInput, type: 'number' });
+export const TextField = class extends Field {
+  constructor(options) {
+		options.data = mergeState(options.data,  { fieldtype: TextInput });
 		super(options);
 	}    
 }
 
-export const MaskedField = class extends Field {    
-    constructor(options) {
-		options.data = mergeComponentState(options.data,  { fieldtype: MaskedInput });
+export const NumberField = class extends Field {
+  constructor(options) {
+		options.data = mergeState(options.data,  { fieldtype: NumberInput, type: 'number' });
 		super(options);
 	}    
 }
 
-export const CurrencyField = class extends Field {    
-    constructor(options) {
-		options.data = mergeComponentState(options.data,  { fieldtype: CurrencyInput });
+export const MaskedField = class extends Field {
+  constructor(options) {
+		options.data = mergeState(options.data,  { fieldtype: MaskedInput });
 		super(options);
 	}    
 }
 
-export const SelectField = class extends Field {    
-		constructor(options) {
-		options.data = mergeComponentState(options.data,  { fieldtype: SelectInput });
+export const CurrencyField = class extends Field {
+  constructor(options) {
+		options.data = mergeState(options.data,  { fieldtype: CurrencyInput });
+		super(options);
+	}    
+}
+
+export const SelectField = class extends Field {
+	constructor(options) {
+		options.data = mergeState(options.data,  { fieldtype: SelectInput });
 		super(options);
 	}    
 }
